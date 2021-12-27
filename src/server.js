995a3e36e82f7c06fc0de5847758a92ce034220b
Melih <ahmtmlh@gmail.com>
2021-12-27 20:53:21 +0300

    Kod düzenlemesi

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
 app.listen(8080, () => {
-    console.log('Application is running on: ', process.env.APP_PORT)
+    console.log('Application is running on: ', 8080)
 })
 
